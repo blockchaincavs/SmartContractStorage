ab203ed038e37fb8a3abb409088b6b438343b5fd
blockchaincavs <blockchaincavs@gmail.com>
2025-06-12 09:54:41 -0400

    updated foundry lib and toml for ignoring lints

--- script/DeployStorage.s.sol
+++ script/DeployStorage.s.sol
@@ -6,12 +6,31 @@ import { Storage } from "../src/Storage.sol";
 
 contract DeployStorage is Script {
 
+    address private storageContractAddress;
+
     function run() external {
 
         // anything between startBroadcast and stopBroadcast sends to the rpc
         vm.startBroadcast();
         Storage storageContract = new Storage();
         vm.stopBroadcast();
-        console.log("Contract deployed! Address:", address(storageContract));
+        
+        storageContractAddress = address(storageContract);
+        createAddressConfigFile();
+    }
+
+    /**
+    * @notice Create address config file to be used on UI and backend.
+    */
+    function createAddressConfigFile() private {
+        
+        string memory configFilePath = vm.envString("CONFIG_PATH");
+        
+        // random key to allow mulitple objects to be serialized
+        string memory obj1 = "some key";
+
+        // random key to allow multiple objects to be serialized
+        string memory json = vm.serializeAddress(obj1, "STORAGE_CONTRACT_ADDRESS", storageContractAddress);
+        vm.writeJson(json, configFilePath);
     }
 }
